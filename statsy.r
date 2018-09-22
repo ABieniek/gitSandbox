5783467f9e55a9746a21091d8aaa18e3faddbd71
abieni3 <abieni3@illinois.edu>
2018-09-22 10:42:21 -0500

    added aggregate binomialdistribution probability

--- statsy.r
+++ statsy.r
@@ -2,3 +2,10 @@ pBinomial = function(trialCount, probability, successCount) {
 	return(choose(trialCount, successCount)*(probability**successCount)*((1-probability)**(trialCount-successCount)))
 }
 
+aggPBinomial = function(trialCount, probability, successCountRange) {
+	agg = 0
+	for (c in successCountRange)
+		agg = agg + pBinomial(trialCount, probability, c)
+	return(agg)
+}
+
